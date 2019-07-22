0b8ae54a4bef7193474808cf9c2e9fcb860550e9
Geoffrey C. Stentiford <18690162+geoffreycs@users.noreply.github.com>
2019-07-22 17:01:04 +0700

    Added new search rules, legal disclaimers

--- content.js
+++ content.js
@@ -1,84 +1,143 @@
+//Written by geoffreycs
+
 function urlGetter() {
-    var urlFound = false;
-    var videoURL = "";
-    try {
-        //if (urlFound == false) {
+    if (location.href.indexOf("www.nejm.org") == -1) { //Sanity check 1: Is this even a NEJM page?
+        videoURL = "invalid"
+    } else if (location.href.indexOf("/doi/") == -1) { //Sanity check 2: Is this a support "doi" page?
+        videoURL = "unsupported"
+    } else { //Sanity checks passed, begin searching through page as per rules
+        var urlFound = false;
+        var videoURL = "";
+        try {
+            //if (urlFound == false) {
             videoURL = document.querySelector("#article_body > aside > p > a").getAttribute("data-mobileurl")
-            urlFound = true;
-        //}
-    } catch (e) {
-        console.log("Not found under #article_body > aside > p > a");
-    }
-    try {
-        if (urlFound == false) {
-            videoURL = document.querySelector("#article_introduction > div > div > aside > p > a").getAttribute("data-mobileurl")
-            urlFound = true;
+            if (videoURL.indexOf(".mp4") !== -1) { //Make sure we found an MP4 video, not an MP3 track
+                urlFound = true;
+            }
+            //}
+        } catch (e) {
+            console.log("Not found under #article_body > aside > p > a");
         }
-    } catch (e) {
-        console.log("Not found under #article_introduction > div > div > aside > p > a")
-    }
-    try {
-        if (urlFound == false) {
-            videoURL = document.querySelector("#article__s001 > div > div > aside > p > a").getAttribute("data-mobileurl")
-            urlFound = true;
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("#article_introduction > div > div > aside > p > a").getAttribute("data-mobileurl")
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under #article_introduction > div > div > aside > p > a")
         }
-    } catch (e) {
-        console.log("Not found under #article__s001 > div > div > aside > p > a")
-    }
-    try {
-        if (urlFound == false) {
-            videoURL = document.querySelector("#full > section:nth-child(2) > aside > p > a").getAttribute("data-mobileurl")
-            urlFound = true;
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("#article__s001 > div > div > aside > p > a").getAttribute("data-mobileurl")
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under #article__s001 > div > div > aside > p > a")
         }
-    } catch (e) {
-        console.log("Not found under #full > section:nth-child(2) > aside > p > a")
-    }
-	try {
-        if (urlFound == false) {
-            videoURL = document.querySelector("#article_figures > ol > li.m-media-item.m-audio-video > a").getAttribute("data-mobileurl")
-            urlFound = true;
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("#full > section:nth-child(2) > aside > p > a").getAttribute("data-mobileurl")
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under #full > section:nth-child(2) > aside > p > a")
         }
-    } catch (e) {
-        console.log("Not found under #article_figures > ol > li.m-media-item.m-audio-video > a")
-    }
-	try {
-        if (urlFound == false) {
-            videoURL = document.querySelector("#article__sc1 > div > div > aside > p > a.m-media-item__link.js__audioWindow").getAttribute("data-mobileurl")
-            urlFound = true;
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("#article_figures > ol > li.m-media-item.m-audio-video > a").getAttribute("data-mobileurl")
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under #article_figures > ol > li.m-media-item.m-audio-video > a")
         }
-    } catch (e) {
-        console.log("Not found under #article__sc1 > div > div > aside > p > a.m-media-item__link.js__audioWindow")
-    }
-    try {
-        if (urlFound == false) {
-            videoURL = document.querySelector("body > table > tbody > tr:nth-child(2024) > td.line-content > span:nth-child(19) > span:nth-child(6)").innerText
-            urlFound = true;
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("#article_introduction > div > div > span > p > a").getAttribute("data-mobileurl")
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under #article_introduction > div > div > span > p > a")
         }
-    } catch (e) {
-        console.log("Not found under body > table > tbody > tr:nth-child(2024) > td.line-content > span:nth-child(19) > span:nth-child(6)")
-    }
-    try {
-        if (urlFound == false) {
-            videoURL = document.querySelector("body > table > tbody > tr:nth-child(2035) > td.line-content > span:nth-child(10) > span:nth-child(6)").innerText
-            urlFound = true;
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("#article_figures > ol > span > p > a").getAttribute("data-mobileurl")
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under #article_figures > ol > span > p > a")
         }
-    } catch (e) {
-        console.log("Not found under body > table > tbody > tr:nth-child(2035) > td.line-content > span:nth-child(10) > span:nth-child(6)")
-        console.log("Out of ideas.");
-        videoURL = "null";
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("#article__sc1 > div > div > aside > p > a.m-media-item__link.js__audioWindow").getAttribute("data-mobileurl")
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under #article__sc1 > div > div > aside > p > a.m-media-item__link.js__audioWindow")
+        }
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("body > table > tbody > tr:nth-child(2024) > td.line-content > span:nth-child(19) > span:nth-child(6)").innerText
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under body > table > tbody > tr:nth-child(2024) > td.line-content > span:nth-child(19) > span:nth-child(6)")
+        }
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("#article_introduction > div > div > aside:nth-child(2) > p > a").getAttribute("data-mobileurl")
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under #article_introduction > div > div > aside:nth-child(2) > p > a")
+        }
+        try {
+            if (urlFound == false) {
+                videoURL = document.querySelector("body > table > tbody > tr:nth-child(2035) > td.line-content > span:nth-child(10) > span:nth-child(6)").innerText
+                if (videoURL.indexOf(".mp4") !== -1) {
+                    urlFound = true;
+                }
+            }
+        } catch (e) {
+            console.log("Not found under body > table > tbody > tr:nth-child(2035) > td.line-content > span:nth-child(10) > span:nth-child(6)")
+            console.log("Out of ideas."); //We've run through every single test pattern and no URL was found.
+            videoURL = "null";
+        }
+
     }
-    return videoURL
+    return videoURL //Return the results
 }
 
-chrome.runtime.onMessage.addListener(
+/* //Old implementation
+chrome.runtime.onMessage.addListener( //Set up handler for the message from extension toolbar
     function(message, sender, sendResponse) {
-		console.log("Message received from popup");
-        sendResponse({response: urlGetter()});
+        //console.log("Message received from popup"); //For testing only
+        sendResponse({
+            response: urlGetter() //Send back our results
+        });
     }
 );
+*/
 
-chrome.runtime.onConnect.addListener((port) => {
-  port.onMessage.addListener((msg) => {
-     port.postMessage(urlGetter());
-  });
+chrome.runtime.onConnect.addListener((port) => { //Set up handler for the message from extension toolbar
+    port.onMessage.addListener((msg) => {
+        port.postMessage(urlGetter()); //Send back our results
+    });
 });
--- popup.js
+++ popup.js
@@ -1,34 +1,43 @@
-function downloadVideo() {
-	chrome.downloads.download({
-		url: document.getElementById("link").innerText,
-		filename: "Video.mp4" // Optional
-	});
-}
-
-function comURL() {
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    const port = chrome.tabs.connect(tabs[0].id);
-	port.onMessage.addListener(function(response) {
-        var videoURL = response;
-		var displayOut;
-		if (videoURL == "null") {
-			displayOut = "No video URL was found on this page. This doesn't mean that there isn't one, just that the extension was not able to scrape a link off of the page. This extension only works on NEJM pages."
-		} else {
-			displayOut = "<a id='link' target='_blank' href='" + videoURL + "'>" + videoURL + "</a><br/><br/><button id='trigger' type='button'>Download file</button><br/><br/><video src='" + videoURL + "' controls>Video.</video>"
-		}
-		document.getElementById("out").innerHTML = displayOut;
-		if (videoURL != "null") {
-			document.getElementById("trigger").addEventListener("click", downloadVideo)
-		}
+function downloadVideo() { //Used for the "Download file" button that appears
+    chrome.downloads.download({
+        url: document.getElementById("link").innerText, //Grab the link again from the DOM
+        filename: "Video.mp4" //Default filename
     });
-	port.postMessage(null);
-});
 }
 
-window.addEventListener('load', (event) => {
-	try {
-		chrome.tabs.executeScript(null, {file: 'content.js'}, comURL());
-	} catch(e) {
-		document.getElementById("out").innerHTML = "Unable to inject the script. This extension only works on NEJM pages."
-	}
+function comURL() { //Stands for "communicate URL," runs once content script is injected
+    chrome.tabs.query({ //Get list of tabs (only returns active one due to permissions)
+        active: true,
+        currentWindow: true
+    }, function(tabs) { //Callback once we have the current tab
+        const port = chrome.tabs.connect(tabs[0].id); //Open the communication port
+        port.onMessage.addListener(function(response) { //Prepare to handle the response
+            var videoURL = response;
+            var displayOut;
+            if (videoURL == "null") { //If we actually couldn't find the video URL
+                displayOut = "No video URL was found on this page. This doesn't mean that there isn't one, just that the extension was not able to scrape a link off of the page. This extension only works on NEJM pages."
+            } else if (videoURL == "invalid") { //If we're not even on an NEJM page
+                displayOut = "The current webpage is not an NEJM article."
+            } else if (videoURL == "unsupported") { //If we don't support the current page type (not a DOI article page)
+                displayOut = "The extension does not support this type of NEJM page."
+            } else { //If it passed all the sanity checks
+                displayOut = "<a id='link' target='_blank' href='" + videoURL + "'>" + videoURL + "</a><br/><br/><button id='trigger' type='button'>Download file</button><br/><br/><video src='" + videoURL + "' controls>Video.</video>"
+            }
+            document.getElementById("out").innerHTML = displayOut; //Write whatever the output was to DOM
+            if (videoURL != "null") {
+                document.getElementById("trigger").addEventListener("click", downloadVideo) //Hook downloadVideo() to the download button
+            }
+        });
+        port.postMessage(null); //Trigger the content script
+    });
+}
+
+window.addEventListener('load', (event) => { //Make sure that the DOM is fully constructed
+    try {
+        chrome.tabs.executeScript(null, { //Inject the content script
+            file: '/content.js'
+        }, comURL()); //Add the callback to talk to the injected script
+    } catch (e) { //Prevent uncaught errors
+        document.getElementById("out").innerHTML = "Unable to inject script."
+    }
 });
